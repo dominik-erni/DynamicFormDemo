c6d5971a6b4001f503cccd0b4eba83b6a7cd83da
Dominik Erni <dominik.erni@gmail.com>
2013-02-08 13:50:18 +0100

    better layout and new dummy questions

--- DynamicFormJSFDemo/src/com/zuehlke/dynamicform/healthquestions/service/DummyHealthQuestionService.java
+++ DynamicFormJSFDemo/src/com/zuehlke/dynamicform/healthquestions/service/DummyHealthQuestionService.java
@@ -7,14 +7,26 @@ import com.zuehlke.dynamicform.healthquestions.model.HealthQuestion;
 
 public class DummyHealthQuestionService implements HealthQuestionService {
 
+	private static final boolean NOT_BASE_QUESTION = false;
+	private static final boolean TEXT_QUESTION = false;
+	private static final boolean YES_NO_QUESTION = true;
+
 	@Override
 	public Collection<HealthQuestion> loadHealthQuestions() {
 		Collection<HealthQuestion> healthQuestions = new ArrayList<HealthQuestion>();
-		healthQuestions.add(new HealthQuestion("smoke", "Rauchen Sie?", false,
-				null, true));
+		healthQuestions.add(new HealthQuestion("smoke", "Rauchen Sie?",
+				NOT_BASE_QUESTION, null, YES_NO_QUESTION));
 		healthQuestions.add(new HealthQuestion("medicalTreatment",
-				"Sind Sie zur Zeit in ärztlicher Behandlung?", false, null,
-				true));
+				"Sind Sie zur Zeit in ärztlicher Behandlung?",
+				NOT_BASE_QUESTION, null, YES_NO_QUESTION));
+		healthQuestions.add(new HealthQuestion("kids",
+				"Wie viele Kinder haben Sie?", NOT_BASE_QUESTION, null,
+				TEXT_QUESTION));
+		healthQuestions.add(new HealthQuestion("wife",
+				"Wie viele Frauen haben Sie?", NOT_BASE_QUESTION, null,
+				TEXT_QUESTION));
+		healthQuestions.add(new HealthQuestion("sick", "Sind Sie krank?",
+				NOT_BASE_QUESTION, null, YES_NO_QUESTION));
 		return healthQuestions;
 	}
 
